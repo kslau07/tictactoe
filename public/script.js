b110f32fab5536f85f45aaf88f7750273f9a9369
sam <abluecar@gmail.com>
2024-02-20 21:07:36 -0500

    feat: Validate input and cell number

--- public/script.js
+++ public/script.js
@@ -1,14 +1,8 @@
-// Current domain objects:
-// Cell,
-// gameboard,
-// displayController,
-// playerFactory,
-// gameController
-
 const Cell = function () {
-  const value = "o";
+  let value = "F";
   const getValue = () => value;
-  return { getValue };
+  const setValue = (mark) => (value = mark);
+  return { getValue, setValue };
 };
 
 const gameboard = (function () {
@@ -24,10 +18,22 @@ const gameboard = (function () {
   }
 
   const getBoard = () => board;
-  const markCell = function () {
-    console.log("Now check if space is available on board / in cell");
+  const flatBoard = board.flat();
+  const isValidCell = (index) => {
+    const targetCell = flatBoard[index];
+
+    if (targetCell.getValue() == "F") {
+      return true;
+    } else {
+      console.log("This spot has already been taken!");
+      return false;
+    }
   };
-  return { getBoard, markCell };
+  const markCell = function (index, player) {
+    const targetCell = flatBoard[index];
+    targetCell.setValue(player.marker);
+  };
+  return { getBoard, isValidCell, markCell };
 })();
 
 const displayController = (function (gb) {
@@ -48,27 +54,56 @@ const playerProto = {
     console.log(`${this.name} says, "Haha, I am going to win!"`);
   },
 };
-function playerFactory(name) {
+function playerFactory(name, marker) {
   // Note that Object.create / factory functions are considered outdated
   let player = Object.create(playerProto);
   player.name = name;
+  player.marker = marker;
   return player;
 }
 
 const gameController = (function (gameboard, displayController) {
-  const playerOne = playerFactory("Steve");
-  const playerTwo = playerFactory("Mary");
+  const playerOne = playerFactory("Steve", "X");
+  const playerTwo = playerFactory("Mary", "O");
   const { printBoard } = displayController;
-  const { markCell } = gameboard;
+  const { isValidCell, markCell } = gameboard;
+  let currentPlayer;
+  const getCurrentPlayer = () => currentPlayer;
+
+  const switchPlayer = () =>
+    (currentPlayer = getCurrentPlayer() == playerOne ? playerTwo : playerOne);
 
   const newGame = () => {
-    console.log(`Begin new game, it's ${playerOne.name}'s turn!`);
-    printBoard();
+    switchPlayer();
+    // TODO: We could prompt for each player's name here
+    console.log(`New game started!`);
+  };
+
+  const isValidInput = (input) => {
+    const isnum = /^[1-9]$/.test(input);
+    if (!isnum) return false;
+
+    return true;
   };
 
   const playRound = () => {
-    const answer = prompt("Please enter a space # 0-9");
-    markCell()
+    if (typeof currentPlayer == "undefined")
+      return console.log("Please use .newGame() to begin a new game!");
+
+    console.log(`It's ${currentPlayer.name}'s turn!`);
+    printBoard();
+
+    let input;
+    while (true) {
+      input = prompt("Please choose a space between 1-9");
+      if (!isValidInput(input)) continue;
+      index = Number(input) - 1;
+      if (isValidCell(index)) break;
+    }
+
+    markCell(index, currentPlayer);
+    switchPlayer();
   };
+
   return { newGame, playRound };
 })(gameboard, displayController);
